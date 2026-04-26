97afe8775d5adf2fb38c9b1766e7f2330f4fd8ee
JulianEduardoMendez <juliane.mendezp@gmail.com>
2026-04-26 10:19:47 -0500

    Delete matrices directory

--- matrices/main.py
+++
@@ -1,5 +0,0 @@
-def iniciar_programa():
-  from proyecto_matrices import menu as mn
-  from proyecto_matrices import entrada as ent
-  from proyecto_matrices import operaciones_matrices as om
-  mn.mostrar_menu()
