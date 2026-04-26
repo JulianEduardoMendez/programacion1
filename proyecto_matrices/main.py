e28eb4bdbdcc0d6f3ddbc805d0d7120b32d32ec2
JulianEduardoMendez <juliane.mendezp@gmail.com>
2026-04-26 13:25:33 -0500

    Update main.py

--- proyecto_matrices/main.py
+++ proyecto_matrices/main.py
@@ -1,6 +1,6 @@
+from proyecto_matrices import menu as mn
+from proyecto_matrices import entrada as ent
+from proyecto_matrices import operaciones_matrices as om
 def iniciar_programa():
-  from proyecto_matrices import menu as mn
-  from proyecto_matrices import entrada as ent
-  from proyecto_matrices import operaciones_matrices as om
   mn.mostrar_menu()
   pass
